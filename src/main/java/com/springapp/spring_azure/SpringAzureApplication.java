b7825e1afc3a7f9232a3a8414600d6bd13e1d0e3
Ishan Shah <the_ishana@MacBookPro.lan>
2025-06-04 21:00:19 -0500

    first commit

---
+++ src/main/java/com/springapp/spring_azure/SpringAzureApplication.java
@@ -0,0 +1,20 @@
+package com.springapp.spring_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureApplication {
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats! your app deployed successfully in Azure!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/spring_azure/SpringAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.spring_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
